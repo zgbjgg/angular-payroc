7d7a219fea5a7769e61dab5bce5754c33465a252
zgbjgg <zgbjgg@gmail.com>
2016-09-22 15:36:55 -0500

    use node-payroc lib

--- examples/server.js
+++ examples/server.js
@@ -2,16 +2,14 @@ var express = require('express')
 var fs = require('fs')
 var path = require('path')
 var bodyParser = require('body-parser')
-var request = require('request')
-var js2xmlparser = require("js2xmlparser");
-var parseString = require('xml2js').parseString;
+var payroc = require('node-payroc')
 var app = express()
 
-var gateway = {
+payroc.configure({
   apiKey: '2F822Rw39fx762MaV7Yy86jXGTC7sCDy',
   url: 'https://payroc.transactiongateway.com/api/v2/three-step',
   redirectUrl: 'http://127.0.0.1:8000/token'
-}
+})
 
 var angularStr = fs.readFileSync(path.resolve(__dirname, '../bower_components/angular/angular.js'), 'utf8')
 var payrocAngularStr = fs.readFileSync(path.resolve(__dirname, '../dist/payroc-angular.js'), 'utf8')
@@ -29,25 +27,11 @@ app.post('/step-one-path', function (req, res) {
   /* recover body (json) */
   var jsonBody = req.body;
 
-  /* add api key and redirect url */
-  jsonBody['api-key'] = gateway.apiKey;
-  jsonBody['redirect-url'] = gateway.redirectUrl;
-
-  // to xml
-  var xmlRequest = js2xmlparser.parse("sale", jsonBody);
-
-  // send request
-  request.post({
-    url: gateway.url,
-    body : xmlRequest,
-    headers: {'Content-Type': 'text/xml'}
-  }, function (error, response, body) {        
-    if (!error && response.statusCode == 200) {
-      parseString(body, function(err, result) {
-        res.send(result);
-      });
+  payroc.step_one(jsonBody, function(err, result) {
+    if ( err ) {
+      res.send({error: 'error'});
     } else {
-      console.log(error);
+      res.send(result);
     }
   });
 })
@@ -55,21 +39,14 @@ app.post('/step-one-path', function (req, res) {
 app.post('/step-two-path', function(req, res) {
   /* recover body in url encoded */
   var urlEncoded = req.body;
-  var formUrl = urlEncoded['form-url'];
-  delete urlEncoded['form-url'];
 
-  // send request
-  request.post({
-    url: formUrl, 
-    form: urlEncoded
-  }, function(error, response, body) { 
-    if (!error && response.statusCode === 302) {
-      // send redirect location back to client
-      res.send({location: response.headers.location});
+  payroc.step_two(urlEncoded, function(err, result) {
+    if ( err ) {
+      res.send({error: 'error'});
     } else {
-      console.log(error);
+      res.send(result);
     }
-  }); 
+  });
 })
 
 app.get('/token', function (req, res) {
